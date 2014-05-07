19b4f21464a35616ab9ab06d69dff095a5757cf8
Joss Mackison <joss.mackison@gmail.com>
2014-05-07 21:38:10 +1000

    Added a handler for testing emails

--- routes/dev/email.js
+++ routes/dev/email.js
@@ -1,21 +1,90 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+	Topic = keystone.list('Topic'),
+	Reply = keystone.list('Reply');
 
-module.exports = function(req, res) {
+/**
+	Route handler for testing emails
+*/
+
+exports = module.exports = function(req, res) {
 	
-	res.send("HI");
-	
-	// send email
-	new keystone.Email('notification-new-reply').send({
-		// reply: newReply,
-		link: 'http://forum.keystonejs.com' + locals.topic.url,
-		subject: '[KeystoneJS]' + locals.topic.name
-	}, {
-		to: 'joss.mackison@gmail.com',
-		from: {
-			name: 'KeystoneJS Forum',
-			email: 'forum@keystonejs.com'
+	var data = {};
+
+	// save callback
+
+	var renderCallback = function(err, email) {
+		if (err) {
+			res.status(500).render('500', {
+				req: req,
+				env: process.env.NODE_ENV,
+				errorTitle: 'Error rendering email template ' + req.params.key,
+				errorMessage: err
+			});
+		} else {
+			res.send(email.html);
 		}
-	});
+	};
+	
+	
+	// LOAD a random Topic
+
+	var loadTopic = function(callback) {
+		Topic.model.findOne()
+			.where('author').ne(null)
+			.where('state', 'published')
+			.sort('-createdAt')
+			.exec(function(err, topic) {
+				
+				data.topic = topic;
+				callback();
+				
+			});
+	}
+	
+	
+	// LOAD a random Reply
+
+	var loadReply = function(callback) {
+		Reply.model.findOne()
+			.where('author').ne(null)
+			.where('state', 'published')
+			.sort('-createdAt')
+			.exec(function(err, reply) {
+				
+				data.reply = reply;
+				callback();
+				
+			});
+	}
+	
+	
+	switch (req.params.key) {
+
+		case 'forgotten-password':
+			
+			var email = new keystone.Email('forgotten-password');
+			
+			email.render({
+				subject: 'Forgotten Password',
+			}, renderCallback);
+			
+		break;
+
+		case 'notification-new-reply':
+			
+			var email = new keystone.Email('notification-new-reply');
+			
+			loadReply(function() {
+				email.render({
+					subject: 'KeystoneJS reply',
+					reply: data.reply,
+				}, renderCallback);
+			});
+			
+		break;
+
+		default:
+			res.status(404).render('404', { req: req, env: process.env.NODE_ENV });
+	}
 	
 }
-	
